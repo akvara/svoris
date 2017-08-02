cf5a69a175e52f1ebfd441f2c2d61a8915cdf6c8
Andrius K <akvara@gmail.com>
2017-08-02 14:10:28 +0300

    Log1

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,6 @@ def create_app(config_name):
 
     logging.debug('This message should go to the log file')
     logging.info('So should this')
-    logging.warning('And this, too')
     logging.warning(config_name)
 
     app.config.from_object(app_config[config_name])
