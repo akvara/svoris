1e46527c9bd05c3fd666397c0136f557d5440ac0
Andrius K <akvara@gmail.com>
2017-08-03 22:32:37 +0300

    frontend added

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import WeightGet from './WeightGet';
 import WeightPost from './WeightPost';
+import Weight from './Weight';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Svoris</h1>
-                <br />
-                <WeightGet />
-                <br />
-                <WeightPost />
+                <Weight />
             </div>
         );
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/App';
 import WeightPost from './components/weight-post';
 import WeightGet from './components/weight-get';
 import PressureGet from './components/pressure-get';
+import App from './components/App';
 
 // window.onbeforeunload = function() {
    // return "Do you really want to leave Pomodoro app?";
@@ -16,3 +16,4 @@ ReactDOM.render(<WeightPost/>, document.getElementById('weight-post'));
 ReactDOM.render(<WeightGet/>, document.getElementById('weight-get'));
 ReactDOM.render(<WeightPost/>, document.getElementById('pressure-post'));
 ReactDOM.render(<PressureGet/>, document.getElementById('pressure-get'));
+//ReactDOM.render(<App/>, document.getElementById('app'));
