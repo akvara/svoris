2cbe272a65f4a8d30eb57f17a57f98909bf5b39d
Andrius K <akvara@gmail.com>
2017-08-08 06:15:46 +0300

    Imports renamed

--- imports/import_pressures.py
+++ imports/import_pressures.py
@@ -36,12 +36,15 @@ url = 'http://127.0.0.1:5000/pressures/'
 
 if __name__ == "__main__":
     # execute only if run as a script
-    arg_names = ['command', 'input']
-    args = dict(zip(arg_names, sys.argv))
-    if not 'input' in args:
-        print "Usage: " + args["command"] + " input"
-        exit(-1)
-    input_file = args["input"]
+
+    # arg_names = ['command', 'input']
+    # args = dict(zip(arg_names, sys.argv))
+    # if not 'input' in args:
+        # print "Usage: " + args["command"] + " input"
+        # exit(-1)
+    # input_file = args["input"]
+
+    input_file = 'history_pressures.txt'
     try:
         file = open(input_file,"r")
         data = file.read()
--- imports/import_svoriai.py
+++ imports/import_svoriai.py
@@ -44,12 +44,15 @@ url = 'http://svoris-api.herokuapp.com/weights/'
 
 if __name__ == "__main__":
     # execute only if run as a script
-    arg_names = ['command', 'input']
-    args = dict(zip(arg_names, sys.argv))
-    if not 'input' in args:
-        print "Usage: " + args["command"] + " input"
-        exit(-1)
-    input_file = args["input"]
+
+    # arg_names = ['command', 'input']
+    # args = dict(zip(arg_names, sys.argv))
+    # if not 'input' in args:
+        # print "Usage: " + args["command"] + " input"
+        # exit(-1)
+    # input_file = args["input"]
+
+    input_file = 'history_weights.txt'
     try:
         file = open(input_file,"r")
         data = file.read()
